506b27e1d03709ae95710614bd5036b54f22bf73
Jacielio Queiroz <cielioqueiroz@hotmail.com>
2024-04-12 18:58:05 -0300

    projeto-weather

---
+++ main.js
@@ -0,0 +1,11 @@
+const baseURL = "https://api.openweathermap.org/data/2.5/"
+const path = `weather?lat=${lat}&lon=${lon}&appid=${API key}`
+
+
+function main(){
+  console.log("carregou!!")
+
+}
+
+main();
+
