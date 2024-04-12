7c85aea1a2f8295136419df1dd5968fc4aab873e
Jacielio Queiroz <cielioqueiroz@hotmail.com>
2024-04-12 20:29:50 -0300

    configurando API

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-const baseURL = "https://api.openweathermap.org/data/2.5/"
-const path = `weather?lat=${lat}&lon=${lon}&appid=${API key}`
+import { environments } from "./config/environment.js";
 
+const location = document.getElementById("location");
+const search = document.getElementById("search");
 
-function main(){
-  console.log("carregou!!")
+search.addEventListener("click", () => {
+  const city = location.value;
+  console.log(city);
 
-}
+  const API_KEY = environments.apiKey;
+  const baseURL = "https://api.openweathermap.org/data/2.5/";
+  const path = `weather?q=${city}&units=metric&appid=${API_KEY}&lang=pt_BR`;
 
-main();
+  function main() {
+    const url = `${baseURL}${path}`;
+    fetch(url) //fetch serve para conectar com outras API
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err));
+  }
 
+  main();
+});
